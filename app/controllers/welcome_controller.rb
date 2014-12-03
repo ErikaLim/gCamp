4794f82f39c2b5f9d7fd3529061dc8c4fd2372a7
ErikaLim <Erika.megan.lim@gmail.com>
2014-12-03 08:38:12 -0800

    fixed faqs

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,7 +4,7 @@ class WelcomeController < ApplicationController
    @quotes = [
        ['"Failure is not an option. Everyone has to Succeed."', '- Arnold Schwarzenegger'],
        ['"Your time is limited, so don\'t waste it living someone else\'s life"', '- Steve Jobs'],
-       ['"Better Ingredients, Better Pizza"', '- Papa John\'s',],
+       ['"Better Ingredients, Better Pizza"', '- Papa John',],
      ]
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  resources :tasks
+
   root to: "welcome#home"
 
   get 'about' => "pages#about"

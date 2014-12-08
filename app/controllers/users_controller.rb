67d128af72cc58b75bd6fdc5b66f0c6ac33609e5
ErikaLim <Erika.megan.lim@gmail.com>
2014-12-07 21:33:24 -0800

    fixed index link

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
     @user = User.create(user_params)
     respond_to do |format|
       if @user.save
-        format.html {redirect_to @user, notice: 'User was successfully created.'}
+        format.html {redirect_to users_path, notice: 'User was successfully created.'}
       else
         format.html {render.new}
       end

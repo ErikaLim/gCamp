372748c29ba84de7b3943837ab2c376855d86ba9
ErikaLim <Erika.megan.lim@gmail.com>
2014-12-17 11:39:25 -0800

    fixed capybara for tasks

--- spec/features/tasks_spec.rb
+++ spec/features/tasks_spec.rb
@@ -3,17 +3,15 @@ require 'rails_helper'
 feature "Tasks" do
 
   scenario "User makes a task" do
-    Task.create!(
-      description: "Finish gCamp"
-    )
 
     visit tasks_path
-    expect(page).to have_content("Finish gCamp")
     click_on "New Task"
     fill_in "Description", with: "Finish gCamp"
+    fill_in "Due date", with: "01/01/2015"
     click_on "Create Task"
 
     expect(page).to have_content("Finish gCamp")
+    expect(page).to have_content("01/01/2015")
   end
 
   scenario "User wants to see a task" do
@@ -35,10 +33,12 @@ feature "Tasks" do
     expect(page).to have_content("Finish gCamp")
     click_on "Edit"
     fill_in "Description", with: "Fix gCamp"
+    check "Completed"
     click_on "Update Task"
 
     expect(page).to have_content("Fix gCamp")
     expect(page).to have_no_content("Finish gCamp")
+    expect(page).to have_content("Completed")
   end
 
   scenario "User destroys a task" do
